76c85d2a0654e706689705ef6d938128ffce900f
Mario <mqs@melix.org>
2017-09-21 16:46:40 +0200

    adding examples of workflows in powershell

---
+++ workflows.ps1
@@ -0,0 +1,22 @@
+﻿Workflow Get-BiosWFR{
+    Get-CimInstance Win32_Bios
+}
+
+Workflow Get-CimClassWF {
+    [CmdletBinding()]
+    Param($CimClass = "Win32_Bios")
+
+    Get-CimInstance $CimClass
+}
+
+Get-CimClassWF
+
+Get-CimClassWF -CimClass "Win32_ComputerSystem"
+
+Workflow Get-Bios {
+    Get-CimInstance -ClassName Win32_Bios
+}
+
+Get-Bios -PSComputerName DC1 -PSAuthentication Kerberos -Verbose
+
+#$PSParameterCollection ,,,, Get-Bios -PSParameterCollection $PSParameterCollection -AsJob -JobName 'Job3-5'
