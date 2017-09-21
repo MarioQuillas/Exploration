9215c1f8155e4d111a2613bf91b5dc5e19f3a3ca
Mario <mqs@melix.org>
2017-09-21 17:27:56 +0200

    workflow modules and preference variables

--- workflows.ps1
+++ workflows.ps1
@@ -20,3 +20,41 @@ Workflow Get-Bios {
 Get-Bios -PSComputerName DC1 -PSAuthentication Kerberos -Verbose
 
 #$PSParameterCollection ,,,, Get-Bios -PSParameterCollection $PSParameterCollection -AsJob -JobName 'Job3-5'
+
+Get-Module PSWorkflow -ListAvailable | Import-Module
+Get-Module PSWorkflowUtility -ListAvailable | Import-Module
+
+Get-Command -Module PSWorkflow
+Get-Command -Module PSWorkflowUtility
+
+Explorer $pshome\Modules\psworkflow
+
+Workflow Invoke-Preference {
+    Param($pref)
+    $PSPersistancePreference = $pref
+
+    $i = Get-CimInstance win32_bios
+}
+
+$s = Get-Date ; Invoke-Preference -pref $true
+$e = Get-Date ; "Total time: {0}" -f ($e - $s).TotalMilliseconds
+
+$s = Get-Date ; Invoke-Preference -pref $false
+$e = Get-Date ; "Total time: {0}" -f ($e - $s).TotalMilliseconds
+
+Workflow Chkpt-wf {
+    Get-Process
+
+    foreach ($x in 1..100)
+        {$x; Checkpoint-Workflow; Start-Sleep -Seconds 2}
+}
+
+Chkpt-wf -AsJob
+
+Get-Job | Receive-Job -Keep
+
+Get-Job | Resume-Job
+
+Get-Job | Suspend-Job
+
+
