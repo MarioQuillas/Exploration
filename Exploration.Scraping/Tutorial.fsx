4924f65f195732a7c560c118149bcbe70e033f3c
Mario <mqs@melix.org>
2017-09-25 14:56:53 +0200

    scrapping euronext stock information

--- Exploration.Scraping/Tutorial.fsx
+++ Exploration.Scraping/Tutorial.fsx
@@ -6,4 +6,68 @@
 open FSharp.Data
 open System.Text.RegularExpressions
 
-type TotalEuronext = HtmlProvider<"https://www.euronext.com/fr/products/equities/FR0000120271-XPAR">
+#load "../packages/FSharp.Charting.0.91.1/lib/net45/FSharp.Charting.fsx"
+open FSharp.Charting
+
+//type TotalEuronext_1 =
+//  HtmlProvider<"https://www.euronext.com/fr/products/equities/FR0000120271-XPAR">
+
+type TotalEuronext =
+  HtmlProvider<"https://www.euronext.com/fr/nyx_eu_listings/real-time/quote?isin=FR0000120271&mic=XPAR">
+
+let scrapRealTime = TotalEuronext.GetSample()
+
+scrapRealTime.Tables.Table1.Headers
+scrapRealTime.Tables.Table1.Rows
+scrapRealTime.Tables.Table1.Name
+
+scrapRealTime.Tables.Table2.Headers
+scrapRealTime.Tables.Table2.Rows
+scrapRealTime.Tables.Table2.Name
+
+scrapRealTime.Tables.Table3.Headers
+scrapRealTime.Tables.Table3.Rows
+scrapRealTime.Tables.Table3.Name
+
+scrapRealTime.Tables.Table4.Headers
+scrapRealTime.Tables.Table4.Rows
+scrapRealTime.Tables.Table4.Name
+
+scrapRealTime.Tables.Table5.Headers
+scrapRealTime.Tables.Table5.Rows
+scrapRealTime.Tables.Table5.Name
+
+scrapRealTime.Tables.Table6.Headers
+scrapRealTime.Tables.Table6.Rows
+scrapRealTime.Tables.Table6.Name
+
+scrapRealTime.Tables.Table7.Headers
+scrapRealTime.Tables.Table7.Rows
+scrapRealTime.Tables.Table7.Name
+
+//let htmlTotalEuronext =
+//  HtmlDocument.Load("https://www.euronext.com/fr/nyx_eu_listings/real-time/quote?isin=FR0000120271&mic=XPAR")
+
+//let toto = htmlTotalEuronext.Descendants
+
+//let toto = TotalEuronext.GetSample()
+//toto.Lists.List10.Html
+
+//let qsdf = TotalEuronext().Lists.TOTAL.Values
+
+type NugetStats = 
+  HtmlProvider<"https://www.nuget.org/packages/FSharp.Data">
+// load the live package stats, you get intellisense for this
+let rawStats = NugetStats().Lists.Dependencies.Values
+let rawStats2 = NugetStats().Tables.``Version History``
+// note the backticks (``) for writing natural text in F#!
+
+//let parsedList =
+//  (lovelyPageDoc.CssSelect ".portlet.clearfix.reorderableModule td > font") // using a css selector on a .Parse()ed webpage
+//              |> Seq.map (fun (x:HtmlNode) -> x.InnerText()) //get text of each element
+//              |> Seq.skip 26 //skip some elements
+//              |> Seq.pairwise //convert to pairs (in tuples)
+//              |> Seq.indexed //pair with indicies
+//              |> Seq.filter (fun (i, x) -> i % 2 <> 0) //get rid of every second element
+//              |> Seq.map snd //get the second element of every pairing
+//              |> Seq.toList //convert to list
