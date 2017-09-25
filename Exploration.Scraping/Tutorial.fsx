8b1ab7233c5dfc9cc4a65de9ef25def7cb2f6d6e
Mario <mqs@melix.org>
2017-09-25 13:29:20 +0200

    Suave FSharp server testing and updating of packages

--- Exploration.Scraping/Tutorial.fsx
+++ Exploration.Scraping/Tutorial.fsx
@@ -1,4 +1,4 @@
-﻿#I @"../packages/FSharp.Data.2.3.3/lib/net40"
+﻿#I @"../packages/FSharp.Data.2.4.0/lib/net40"
 #r "FSharp.Data.dll"
 //#load some file
 //#time 
---
+++ Exploration.Suave/Program.fs
@@ -0,0 +1,21 @@
+﻿open Suave
+open Suave.Successful
+open Suave.Filters
+open Suave.Operators
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let qsdf =  path "/hello" >=> OK "Hello, folks"
+let titi =
+    choose
+        [ qsdf
+          path "/goodbye" >=> OK "GoodByeeeee!!!"]
+
+let routes =
+    GET >=> titi
+
+[<EntryPoint>]
+let main argv = 
+    startWebServer defaultConfig routes
+    0 // return an integer exit code
