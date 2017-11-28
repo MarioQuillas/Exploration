c4d8d8b72574deea7d9140583dc7c432537f6af4
Mario <mqs@melix.org>
2017-11-28 19:39:50 +0100

    saving filewriter implementation

--- Exploration.FExploration/Tutorial.fsx
+++ Exploration.FExploration/Tutorial.fsx
@@ -2,14 +2,16 @@
 open System.IO
 open System.Text
 
-let coolFileWriter targetFile maxQueueMessages messageHandler =
+type IFileWriter =
+  inherit System.IDisposable
+  abstract member Write: string -> unit
+
+let simpleFileWriter targetFile maxQueueMessages messageHandler =
   if not (File.Exists(targetFile)) then
     use file = File.Create(targetFile) 
     file |> ignore
 
   let queue = new System.Collections.Generic.Queue<string>()
-  //let timer = new System.Timers.Timer(5.0f)
-  let stopWatch = new System.Diagnostics.Stopwatch()
 
   let flushQueue () =
     let sb = new System.Text.StringBuilder()
@@ -19,36 +21,29 @@ let coolFileWriter targetFile maxQueueMessages messageHandler =
 
     File.AppendAllText(targetFile, sb.ToString())
 
-  let cronTask timeInterval =
-    let timer = new System.Timers.Timer(float timeInterval)
-    timer.AutoReset <- true
-    async {
-      while (true) do
-        if (stopWatch.Elapsed.TotalSeconds > 1.0) then
-          stopWatch.Stop()
-          flushQueue() }
-
-  Async.Start(cronTask 1.0)
-
-  (fun message -> 
-    if (queue.Count = 0) then 
-      stopWatch.Restart()
-    queue.Enqueue(messageHandler message)
-    if queue.Count = maxQueueMessages then
-      flushQueue())
+  { 
+    new IFileWriter with
+    member this.Dispose() =
+      flushQueue() 
+    member this.Write message =
+      queue.Enqueue(messageHandler message)
+      if queue.Count = maxQueueMessages then
+        flushQueue()
+   }
 
 let errorLogger targetFile =
-  coolFileWriter targetFile 5 (fun msg -> sprintf "Error at [%s] : %s" (System.DateTime.UtcNow.ToShortTimeString()) msg)
+  simpleFileWriter targetFile 5 (fun msg -> sprintf "Error at [%s] : %s" (System.DateTime.UtcNow.ToShortTimeString()) msg)
 
-let writer targetFile = coolFileWriter targetFile 2000 id
+let writer targetFile = simpleFileWriter targetFile 20000 id
 
-let testWriter = writer @"D:\test2.txt"
-  
-//let toto = new System.Diagnostics.Stopwatch()
-//toto.Restart()
-//toto.Elapsed.TotalSeconds
-//toto.Stop()
+//let testWriter = writer @"D:\test2.txt"
 
-#timer "on"
-[1..1..99] |> List.map (fun item -> testWriter (sprintf "%d" item)) |> ignore
-#timer "off"
+
+let test (lines) =
+  use testWriter = writer @"D:\test2.txt"
+  [1..1..lines] |> List.map (fun item -> testWriter.Log(sprintf "%d" item)) |> ignore
+
+#time "on"
+let lines = 15//10*1000*1000
+test(lines)
+#time "off"
